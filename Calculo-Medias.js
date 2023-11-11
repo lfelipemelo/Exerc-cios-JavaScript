9df3872a45d900f65eea57074d3c548400368983
Felipe Melo <felipemelo@protonmail.com>
2023-11-11 16:15:36 -0300

    Adicionando Exercício Cálculo de Média

---
+++ Calculo-Medias.js
@@ -0,0 +1,12 @@
+let nota1 = parseFloat(prompt("Digite a primeira nota (entre 0 e 10):"));
+let nota2 = parseFloat(prompt("Digite a segunda nota (entre 0 e 10):"));
+let nota3 = parseFloat(prompt("Digite a terceira nota (entre 0 e 10):"));
+
+if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || nota3 < 0 || nota3 > 10) {
+  console.log("Por favor, insira notas válidas entre 0 e 10.");
+} else {
+
+  var media = (nota1 + nota2 + nota3) / 3;
+
+  console.log("A média das notas é: " + media.toFixed(2));
+}
